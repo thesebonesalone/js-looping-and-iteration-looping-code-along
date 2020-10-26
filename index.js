12af6735fbbcd945b02b948a96f06e782c7e834a
thesebonesalone <duikhead2@gmail.com>
2020-10-26 13:33:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown() {
+   let count = 10;
+    while (count >= 0) {
+        console.log(count);
+        count --;
+    }
+}
+
+function writeCards(names, message) {
+    let new_arr = [];
+    for (let i = 0; i < names.length; i += 1) {
+       new_arr.push("Thank you, " + names[i] + ", for the wonderful " + message + " gift!");
+    }
+    return new_arr;
+}
